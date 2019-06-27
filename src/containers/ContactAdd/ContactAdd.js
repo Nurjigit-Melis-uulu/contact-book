2affc92f8e63cb288a4e05e225a6749249b77b97
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-06-28 00:04:05 +0600

    add check for input

--- src/containers/ContactAdd/ContactAdd.js
+++ src/containers/ContactAdd/ContactAdd.js
@@ -1,13 +1,104 @@
 import React, { Component } from "react";
 import classes from "./ContactAdd.module.css";
-import Form from "../../components/Controls/Form/Form";
 
 class ContactAdd extends Component {
-  state = {};
+  state = {
+    nameCorrect: false,
+    emailCorrect: false,
+    phoneCorrect: false,
+    pictureCorrect: false
+  };
+
+  nameCorrect = event => {
+    if (event.target.value > 0) {
+      this.setState({ nameCorrect: true });
+    } else {
+      this.setState({ nameCorrect: false });
+    }
+  };
+
+  emailCorrect = event => {
+    if (event.target.value > 0) {
+      this.setState({ emailCorrect: true });
+    } else {
+      this.setState({ emailCorrect: false });
+    }
+  };
+
+  phoneCorrect = event => {
+    if (event.target.value > 0) {
+      this.setState({ phoneCorrect: true });
+    } else {
+      this.setState({ phoneCorrect: false });
+    }
+  };
+
+  pictureCorrect = event => {
+    if (event.target.value > 0) {
+      this.setState({ pictureCorrect: true });
+    } else {
+      this.setState({ pictureCorrect: false });
+    }
+  };
+
   render() {
+    let disabled = false;
+    if (
+      this.state.nameCorrect &&
+      this.state.emailCorrect &&
+      this.state.phoneCorrect &&
+      this.state.pictureCorrect
+    ) {
+      disabled = false;
+    } else {
+      disabled = true;
+    }
     return (
       <div className={classes.ContactAdd}>
-        <Form />
+        <div className={classes.form}>
+          <h2>Add new contact</h2>
+          <label htmlFor="name">
+            Name
+            <input
+              onChange={this.nameCorrect}
+              type="text"
+              placeholder="enter name"
+              name="name"
+              required
+            />
+          </label>
+          <label htmlFor="email">
+            Email
+            <input
+              onChange={this.emailCorrect}
+              type="text"
+              placeholder="enter email"
+              name="email"
+              required
+            />
+          </label>
+          <label htmlFor="phone">
+            Phone
+            <input
+              onChange={this.phoneCorrect}
+              type="text"
+              placeholder="enter phone number"
+              name="phone"
+              required
+            />
+          </label>
+          <label htmlFor="picture">
+            Picture
+            <input
+              onChange={this.pictureCorrect}
+              type="text"
+              placeholder="enter link"
+              name="picture"
+              required
+            />
+          </label>
+          <button disabled={disabled}>Add</button>
+        </div>
       </div>
     );
   }
